4891a5bdcae8272df598683aed516611cd2d3cef
caleb <ogundelecaleb13@gmail.com>
2025-11-27 08:13:15 +0100

    xx

--- src/components/Home/Cta.tsx
+++ src/components/Home/Cta.tsx
@@ -6,16 +6,16 @@ export default function Cta() {
   return (
     <section className="w-full bg-gradient-to-r from-blue-600 to-blue-500 overflow-hidden">
       <div className="max-w-7xl mx-auto px-4 md:px-[42px] lg:px-[80px]">
-        <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 lg:gap-12 items-center">
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 md:gap-7 lg:gap-12 items-center">
           {/* Left Content */}
           <div className="text-white py-8 md:py-14 lg:py-18">
             <h1 className="text-2xl sm:text-3xl lg:text-4xl xl:text-5xl font-bold leading-tight mb-4 md:mb-6">
               Enjoy Maximum Shopping Experience Today
             </h1>
-            <p className="text-sm md:text-base lg:text-base text-blue-100 leading-relaxed mb-6 md:mb-8">
+            <p className="text-sm md:text-base lg:text-base text-blue-100 leading-relaxed mb-4 md:mb-8">
               Search, buy and sell your product in any part of the world with us
             </p>
-            <div className="pt-4">
+            <div className="md:pt-2">
               <Button
                 size="lg"
                 className="bg-white text-black  hover:bg-gray-50 text-lg px-8 md:px-14 py-2 md:py-3 rounded-md font-normal transition-all duration-300 hover:scale-105"
@@ -31,7 +31,8 @@ export default function Cta() {
               <img
                 src="/images/womanShop.png"
                 alt="Happy woman shopping with phone and bags"
-                className="w-full object-cover"
+                className="h-28 w-full object-contain md:object-cover
+                md:h-auto md:w-auto"
               />
             </div>
           </div>
