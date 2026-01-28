6b30ad27b6e9ca475b5d4bc8f443e40b873a8072
caleb <ogundelecaleb13@gmail.com>
2026-01-28 18:48:28 +0100

    update

--- src/app/home/checkout/page.tsx
+++ src/app/home/checkout/page.tsx
@@ -184,12 +184,13 @@ export default function CheckoutPage() {
     }
   };
 
-  
+
     const handleProceedToPayment = async () => {
     if (!user) {
       toast.error("You have to be logged in to proceed to checkout");
       return;
     }
+    
 
     const addresses = addressData?.addresses || user?.addresses || [];
     const hasShippingAddress = addresses.some(addr => addr.type === "shipping");
